9ce37d05f5513cb2b568e1d82aa00f773d91f1d3
Kisor-S <kishorevk910@gmail.com>
2024-03-06 15:45:19 +0530

    Create a UI with responsive buttons for RPS game

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
+let player = 0 ; 
+let computer = 0 ; 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-function  playRound (lPlayerSelection, computerSelection){
+
+function getComputerChoice() {
+    let arr = ["rock", "paper", "scissors"];
+    return arr[Math.floor(Math.random()* arr.length)];
+}
+
+function reset() {
+    if(player == 0)
+    {
+        champion.textContent ="";
+        list.appendChild(champion);
+    }
+}
+
+const champion = document.createElement("h1");
+
+function final() {
+    if(Math.max(player, computer)>=5){
+        if(player>computer){
+            champion.textContent = "Congrats! You are the winner";
+            list.appendChild(champion);
+        }
+        else {
+            champion.textContent = "Sorry! Computer wins";
+            list.appendChild(champion);
+        }
+        return computerDisplay.textContent = ("Computer :") +  computer, result.textContent, playerDisplay.textContent = ("Player :") + player, player =0, computer =0;
+    }
+}
+
+function  playRound (lPlayerSelection, computerSelection)
+{
+    
     if(lPlayerSelection === computerSelection)
     {
-         return console.log("It's a tie!");
+        return result.textContent = "It's a tie!";
     }
     else
     {
@@ -10,54 +47,78 @@ function  playRound (lPlayerSelection, computerSelection){
         {
             if(computerSelection === "paper")
             {
-                return console.log("You Lose! Paper beats Rock");
+                return computer ++, result.textContent = "You Lose! Paper beats Rock";
             }
             else
             {
-                return console.log("You Win! Rock beats scissors");
+                return player ++, result.textContent = "You Win! Rock beats scissors";
             }
         }
+
         else if(lPlayerSelection === "paper")
         {
             if(computerSelection === "rock")
             {
-                return console.log("You Win! Paper beats Rock");
+                return player ++, result.textContent = "You Win! Paper beats Rock";
             }
             else
             {
-                return console.log("You Lose! Scissors beats paper");
+            
+                return computer ++, result.textContent = "You Lose! Scissors beats paper";
             }
         }
-        else if(lPlayerSelection === "scissors")
+        else(lPlayerSelection === "scissors")
         {
             if(computerSelection === "paper")
             {
-                return console.log("You Win! Scissors beats paper");
+                return player ++, result.textContent = "You Win! Scissors beats paper";
             }
             else
             {
-                return console.log("You Lose! Rock beats scissors");
+                return computer ++, result.textContent = "You Lose! Rock beats scissors";
             }
         }
-        else {
-            return console.log("Please check your spelling");
-        }
     }
 }
 
-for(i=5; i>=1; i--)
-{
-    let playerSelection = prompt("Please select Rock, Paper or Scissor", "Rock");
-    let lPlayerSelection = playerSelection.toLowerCase();
-    
-    function getComputerChoice() {
-        let arr = ["rock", "paper", "scissors"];
-        return arr[Math.floor(Math.random()* arr.length)];
-    }
-    
+rock.addEventListener("click", () => {
+    lPlayerSelection = "rock";
     let computerSelection = getComputerChoice();
-
     playRound(lPlayerSelection, computerSelection);
-    
-}
+    playerDisplay.textContent = ("Player :") + player;
+    computerDisplay.textContent = ("Computer :") +  computer;
+    reset();
+    final();
+});
 
+paper.addEventListener("click", () => {
+    lPlayerSelection = "paper";
+    let computerSelection = getComputerChoice();
+    playRound(lPlayerSelection, computerSelection);
+    playerDisplay.textContent = ("Player :") + player;
+    computerDisplay.textContent = ("Computer :") +  computer;
+    reset();
+    final();
+});
+
+scissors.addEventListener("click", () => {
+    lPlayerSelection = "scissors";
+    let computerSelection = getComputerChoice();
+    playRound(lPlayerSelection, computerSelection);
+    playerDisplay.textContent = ("Player :") + player;
+    computerDisplay.textContent = ("Computer :") +  computer;
+    reset();
+    final();
+});
+
+const list = document.querySelector("body");
+const result = document.createElement("div");
+list.appendChild(result);
+
+const playerDisplay = document.createElement("div");
+playerDisplay.textContent = ("Player :") + player;
+list.appendChild(playerDisplay);
+
+const computerDisplay = document.createElement("div");
+computerDisplay.textContent = ("Computer :") +  computer;
+list.appendChild(computerDisplay);
